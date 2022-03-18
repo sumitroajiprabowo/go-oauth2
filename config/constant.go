bdafb5ea10621dd0f4f23f0dcfb1a11d1338aef9
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-03-18 22:41:00 +0700

    Edit

--- config/constant.go
+++ config/constant.go
@@ -7,8 +7,8 @@ import (
 
 func SetupConfig() *oauth2.Config {
 	conf := &oauth2.Config{
-		ClientID:     "1074900166822-7v6a3nf33a1ifi3tat37n4aa1is58tng.apps.googleusercontent.com",
-		ClientSecret: "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",
+		ClientID:     "CLIENT_ID",
+		ClientSecret: "SECRET_KEY",
 		RedirectURL:  "http://localhost:8000/google/callback",
 		Scopes: []string{
 			"https://www.googleapis.com/auth/userinfo.email",
